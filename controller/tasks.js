1bc106fe217467890a6aaf17800f08bfc5dc049a
Sonu Acharya <sonuacharya2022@gmail.com>
2024-04-16 21:12:08 +0530

    Get tasks methods added

--- controller/tasks.js
+++ controller/tasks.js
@@ -1,11 +1,27 @@
 const Task = require("./../Models/task");
 
-exports.getAllTasks = (req, res) => {
-  res.send("All Items");
+exports.getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({
+      result: tasks.length,
+      tasks: tasks,
+    });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
 };
 
-exports.getTask = (req, res) => {
-  res.send("Get one task");
+exports.getTask = async (req, res) => {
+  try {
+    const task = await Task.findOne({ _id: req.params.id });
+    if (!task) return res.status(404).json({ Message: "No such task found" });
+    res.status(200).json({
+      task,
+    });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
 };
 
 exports.createTask = async (req, res) => {
@@ -13,7 +29,7 @@ exports.createTask = async (req, res) => {
     const task = await Task.create(req.body);
     res.status(201).json({ task });
   } catch (err) {
-    res.status(500).json({message:err});
+    res.status(500).json({ message: err });
   }
 };
 
